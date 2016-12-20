5a41d0f1803766c7a965cbd82346b34158c26984
erikisaac <erik@erikisaac.com>
2016-12-19 19:48:33 -0700

    Challenge 2 done with the last part blocked.

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,25 @@
 console.log('challenge #2 -- app.js loaded!');
 
+var app = angular.module("appTwo", []);
+
 // var app = angular.module("appTwo", []);
 // app.controller("WelcomeController", WelcomeController);
 
 // function WelcomeController(){
 //   this.full_name = "YOUR NAME"
 // }
+
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+
+function WelcomeController(){
+  this.full_name = "Erik Isaac"; // change me
+  this.age = 44;
+  this.city = "Denver";
+  this.state = "CO";
+  // I don't know why this wont work...
+  // this.shout = functon()
+  // 	{
+  // 		// return shout;
+  // 	};
+}
