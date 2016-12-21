9b0ca979b6247c9f21a8735f0b4502a204082687
erikisaac <erik@erikisaac.com>
2016-12-21 09:30:47 -0700

    Challenge 2 finished.

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -9,7 +9,7 @@ var app = angular.module("appTwo", []);
 //   this.full_name = "YOUR NAME"
 // }
 
-var app = angular.module("appTwo", []);
+// var app = angular.module("appTwo", []);
 app.controller("WelcomeController", WelcomeController);
 
 function WelcomeController(){
@@ -17,9 +17,37 @@ function WelcomeController(){
   this.age = 44;
   this.city = "Denver";
   this.state = "CO";
-  // I don't know why this wont work...
-  // this.shout = functon()
-  // 	{
-  // 		// return shout;
-  // 	};
+  this.shout = function(shout) {
+		shout = shout.toUpperCase() + "!!!";
+		return shout;
+	};
 }
+
+app.controller("SecondController", SecondController);
+
+function SecondController(){
+	console.log("Second controller online!!");
+	this.class_name = "GA WDI";
+	this.enrolled_students = 10;
+	this.start_date = new Date("03/25/2015");
+	this.end_date = new Date("3/25/2016");
+	// NOTE: This is not a readable format but I am not going to spend more time on this becasue I want to finish this homeowrk.
+	this.daysRemaining = function() {
+		console.log("daysRemaining function online!");
+		return this.end_date - this.start_date;
+	};
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
